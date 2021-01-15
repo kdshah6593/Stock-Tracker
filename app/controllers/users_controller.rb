92917b0867d7edd5465ec7704f5e2b6ee848860c
Kunal <kdshah6593@gmail.com>
2021-01-15 14:24:33 -0500

    user sign up views and controller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
 
     def new  #signup
+        @user = User.new
     end
 
     def create
+        @user = User.new(params.require(:user).permit(:first_name, :last_name, :username, :password, :email)
+        @user.save
+        redirect_to watchlists_path
     end
 end
