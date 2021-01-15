933baa1194106d3f25a679ebccb49f9db46a889b
Kunal <kdshah6593@gmail.com>
2021-01-15 15:10:34 -0500

    helper methods for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+    helpers do
+        #want this to evaluate for truthiness, so use double bang
+        def logged_in?
+          !!current_user
+        end
+    
+        #establishes current user using the session id after login request
+        def current_user
+          @current_user ||= @current_user = User.find(session[:user_id]) if session[:user_id]
+        end
+    end
 end
