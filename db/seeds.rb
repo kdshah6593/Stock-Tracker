2d0a65ee7b0b7181decbfc12a17da2ff2cf6194c
Kunal <kdshah6593@gmail.com>
2021-01-15 11:37:33 -0500

    seeded database with stocks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+response = HTTParty.get("https://pkgstore.datahub.io/core/s-and-p-500-companies/constituents_json/data/8fd832966a715a70cb9cf3f723498e3b/constituents_json.json")
+
+response.each do |stock|
+    new_stock = Stock.new
+    new_stock.name = stock["Name"]
+    new_stock.sector = stock["Sector"]
+    new_stock.symbol = stock["Symbol"]
+    new_stock.save
+end
