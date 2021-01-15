64ecf0f1949aebf9b2ac6098e69168d0c4ebac14
Kunal <kdshah6593@gmail.com>
2021-01-15 11:38:23 -0500

    updateonly relevant routes, added login and sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :stocks, only: [:index, :show]
+  resources :watchlists
+  resources :users, only: [:create]
+  get 'signup', to: 'users#new'
+
+  get 'login', to: 'sessions#login'
+  get 'logout', to: 'sessions#logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
