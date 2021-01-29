0f61c3a7a00b975de7bdff31ae970acaabf3d543
Kunal <kdshah6593@gmail.com>
2021-01-29 16:37:54 -0500

    made user_params private, conditional logging in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,14 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new(params.require(:user).permit(:first_name, :last_name, :username, :password, :email))
-        @user.save
-        redirect_to user_path(@user)
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            #NEED TO add @user here so we can use error messaging, which needs to be added to new page
+            render :new
+        end
     end
 
     def show
@@ -28,5 +33,10 @@ class UsersController < ApplicationController
 
     def profile #need to create a route for this still
 
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :username, :password, :email)
     end
 end
