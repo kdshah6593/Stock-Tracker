39a845bcc39a2756eb453431ccf779bae2f711be
Kunal <kdshah6593@gmail.com>
2021-01-29 17:37:07 -0500

    helper methods for current user and logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  helper_method :logged_in?
 
-    helpers do
-        #want this to evaluate for truthiness, so use double bang
-        def logged_in?
-          !!current_user
-        end
-    
-        #establishes current user using the session id after login request
-        def current_user
-          @current_user ||= @current_user = User.find(session[:user_id]) if session[:user_id]
-        end
-    end
+#establishes current user using the session id after login request
+  def current_user
+    @current_user ||= @current_user = User.find(session[:user_id]) if session[:user_id]
+  end
+  
+#want this to evaluate for truthiness, so use double bang
+  def logged_in?
+    !!current_user
+  end
+
 end
