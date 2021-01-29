574b39c11b13b2cc8b97f89a93d7a92359e03516
Kunal <kdshah6593@gmail.com>
2021-01-29 15:20:06 -0500

    stock controller actions stubbed out

--- app/controllers/stock_purchases_controller.rb
+++ app/controllers/stock_purchases_controller.rb
@@ -1,2 +1,18 @@
 class StockPurchasesController < ApplicationController
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def new #this is form to add a stock to watchlist
+
+    end
+
+    def create #this is to add a stock to watchlist
+
+    end
 end
