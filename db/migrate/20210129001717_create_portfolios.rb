8743efeea0f3cd26d11101b53224fe27c45f87f4
Kunal <kdshah6593@gmail.com>
2021-01-28 19:20:28 -0500

    added portfolio to database

--- db/migrate/20210129001717_create_portfolios.rb
+++ db/migrate/20210129001717_create_portfolios.rb
@@ -1,7 +1,9 @@
 class CreatePortfolios < ActiveRecord::Migration[6.1]
   def change
     create_table :portfolios do |t|
-
+      t.string :title
+      t.string :type
+      t.integer :user_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_29_001330) do
+ActiveRecord::Schema.define(version: 2021_01_29_001717) do
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "type"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "stock_purchases", force: :cascade do |t|
     t.integer "amount"
