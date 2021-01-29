885acd480f0c8e970dbeb06450e788052baae8ba
Kunal <kdshah6593@gmail.com>
2021-01-29 12:24:10 -0500

    remove portfolio duplicate routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,28 +5,23 @@ Rails.application.routes.draw do
 
   #portfolios & watchlists
   resources :users, only: [:show] do
-    resources :portfolios
-    resources :watchlists
+    resources :portfolios, except: [:show]
+    resources :watchlists, except: [:show]
   end
 
   #stocks
   resources :stocks, only: [:index, :show]
-  resources :watchlists do
+
+  resources :watchlists, only: [:show] do
     resources :stocks, only: [:new, :create]
   end
   # get '/watchlists/:watchlist_id/stocks/new', to: 'stocks#new', as: 'add_stock'
   # post '/watchlists/:watchlist_id/stocks', to: 'stocks#create', as: ''
 
   #stock_purchases
-  resources :portfolios do
+  resources :portfolios, only: [:show] do
     resources :stock_purchases, only: [:new, :create, :delete]
   end
-  
-  # resources
-  #  :portfolios
-  # resources :stock_purchases
-  # resources :stocks, only: [:index, :show]
-  # resources :watchlists
 
   # get 'signup', to: 'users#new'
 
