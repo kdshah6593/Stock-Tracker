7fb8213456dc3b35edb612eb104a5f1b29552351
Kunal <kdshah6593@gmail.com>
2021-01-29 15:21:15 -0500

    moved stock controller actions to stock controller

--- app/controllers/stock_purchases_controller.rb
+++ app/controllers/stock_purchases_controller.rb
@@ -1,18 +1,3 @@
 class StockPurchasesController < ApplicationController
 
-    def index
-
-    end
-
-    def show
-
-    end
-
-    def new #this is form to add a stock to watchlist
-
-    end
-
-    def create #this is to add a stock to watchlist
-
-    end
 end
--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -7,4 +7,12 @@ class StocksController < ApplicationController
     def show 
         @stock = Stock.find(params[:id])
     end
+
+    def new #this is form to add a stock to watchlist
+
+    end
+
+    def create #this is to add a stock to watchlist
+
+    end
 end
