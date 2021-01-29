e17f2ddbc220a0992a863c91425c745c24e2843e
Kunal <kdshah6593@gmail.com>
2021-01-29 15:13:08 -0500

    stub out portfolio controller actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,29 @@
 class PortfoliosController < ApplicationController
+    def index
+
+    end
+
+    def new 
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def edit 
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+        
+    end
 end
