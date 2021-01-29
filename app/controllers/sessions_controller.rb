98f0f66c978fd8e329e24815c4f68a837c9edd76
Kunal <kdshah6593@gmail.com>
2021-01-29 18:33:37 -0500

    add password authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
     end
 
     def create #login user
-        if @user = User.find_by(username: params[:user][:username])
+    @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
