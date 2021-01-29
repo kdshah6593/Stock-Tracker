9a296ba87dedc65df87f57af4c7a83e6ff5171d3
Kunal <kdshah6593@gmail.com>
2021-01-29 16:47:38 -0500

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
     has_secure_password
     has_many :watchlists
     has_many :portfolios
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
 end
