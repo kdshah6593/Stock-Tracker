33c3ff9da850382af8355e9445829b94cd1c3492
Kunal <kdshah6593@gmail.com>
2021-01-29 16:00:18 -0500

    moved user sign up form to partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new(params.require(:user).permit(:first_name, :last_name, :username, :password, :email)
+        @user = User.new(params.require(:user).permit(:first_name, :last_name, :username, :password, :email))
         @user.save
         redirect_to watchlists_path
     end
