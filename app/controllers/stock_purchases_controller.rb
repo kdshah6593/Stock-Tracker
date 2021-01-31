e3a93afa79ca2e20a8a144e95d67dfa6ab338b2d
Kunal <kdshah6593@gmail.com>
2021-01-31 17:17:23 -0500

    form for stock purchase

--- app/controllers/stock_purchases_controller.rb
+++ app/controllers/stock_purchases_controller.rb
@@ -1,11 +1,11 @@
 class StockPurchasesController < ApplicationController
 
     def new
-        @stock_purchase = StockPurchase.new
+        @portfolio = Portfolio.find_by(id: params[:portfolio_id])
+        @stock_purchase = @portfolio.stock_purchases.build
     end
 
     def create
-
     end
 
     def destroy
