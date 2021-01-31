92cfe35875cb171cb715b84c0149499d7ff9e095
Kunal <kdshah6593@gmail.com>
2021-01-31 15:29:57 -0500

    portfolio validations

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,4 +2,6 @@ class Portfolio < ApplicationRecord
     belongs_to :user
     has_many :stock_purchases
     has_many :stocks, through: :stock_purchases
+    validates :title, presence: true
+    validates :portfolio_type, presence: true
 end
