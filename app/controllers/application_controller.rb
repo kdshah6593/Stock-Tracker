926102103124bfc8394dca9f82d62387fed25167
Kunal <kdshah6593@gmail.com>
2021-01-31 13:26:12 -0500

    verify user before controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
-  helper_method :logged_in?
+  before_action :verified_user
+  helper_method :logged_in?, :current_user, :verified_user
 
 #establishes current user using the session id after login request
   def current_user
@@ -10,6 +10,11 @@ class ApplicationController < ActionController::Base
 #want this to evaluate for truthiness, so use double bang
   def logged_in?
     !!current_user
+  end
+
+#want this to verify if user is logged in otherwise redirect to login
+  def verified_user
+    redirect_to root_path unless logged_in?
   end
 
 end
