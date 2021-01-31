5019e4a5f3f573f87aa467c339f27abf7a3145f5
Kunal <kdshah6593@gmail.com>
2021-01-31 16:07:08 -0500

    error messaging helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+    def display_form_errors(x)
+        if x.errors.any?
+            x.errors.collect do |error|
+                error.full_message
+            end
+        end
+    end
+
+    def display_errors(y)
+        form_errors(y).each do |error|
+            content_tag(:p, error)
+        end
+    end
 end
