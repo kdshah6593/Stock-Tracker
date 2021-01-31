fe336f7ffb4f93cf83ba66e215231774e83a5b3e
Kunal <kdshah6593@gmail.com>
2021-01-31 16:18:57 -0500

    remove custom error helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,2 @@
 module ApplicationHelper
-
-    def display_form_errors(x)
-        if x.errors.any?
-            x.errors.collect do |error|
-                error.full_message
-            end
-        end
-    end
-
-    def display_errors(y)
-        form_errors(y).each do |error|
-            tag.div tag.p(error)
-            tag.br
-        end
-    end
 end
