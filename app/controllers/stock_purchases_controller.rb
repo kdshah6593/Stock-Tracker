38852eea42b6052aecf44e3e12104fafa52c76a6
Kunal <kdshah6593@gmail.com>
2021-01-31 17:28:47 -0500

    stock purchase controller actions and errors

--- app/controllers/stock_purchases_controller.rb
+++ app/controllers/stock_purchases_controller.rb
@@ -6,9 +6,21 @@ class StockPurchasesController < ApplicationController
     end
 
     def create
+        @portfolio = Portfolio.find_by(id: params[:portfolio_id])
+        @stock_purchase = @portfolio.stock_purchases.build(stock_purchase_params)
+        if @stock_purchase.save
+            redirect_to user_portfolio_path(current_user, @stock_purchase.portfolio)
+        else
+            render :new
+        end
     end
 
     def destroy
         
+    end
+
+    private
+    def stock_purchase_params
+        params.require(:stock_purchase).permit(:amount, :cost, :stock_id, :portfolio)
     end
 end
