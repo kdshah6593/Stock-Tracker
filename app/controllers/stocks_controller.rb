36140c706dff0a794a4ba893e18c04956504adb6
Kunal <kdshah6593@gmail.com>
2021-02-01 11:36:11 -0500

    action to save stock to watchlist

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -8,11 +8,17 @@ class StocksController < ApplicationController
         @stock = Stock.find_by(id: params[:id])
     end
 
-    def new #this is form to add a stock to watchlist
-
+    def new #this is form to add a stock to watchlist #possibly don't need since form built into watchlist show page
     end
 
     def create #this is to add a stock to watchlist
-        
+        raise params.inspect
+        @stock_watchlist = StockWatchlist.new()
+    end
+
+
+    private
+    def stock_watchlist_params
+        params.require(:stock_watchlist).permit(:stock_id, :watchlist_id)
     end
 end
