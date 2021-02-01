901018f4c91fc4265b38592a94e689b63ccb94b2
Kunal <kdshah6593@gmail.com>
2021-02-01 16:44:10 -0500

    search stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,4 +3,12 @@ class Stock < ApplicationRecord
     has_many :watchlists, through: :stock_watchlists
     has_many :stock_purchases
     has_many :portfolios, through: :stock_purchases
+
+    def self.search(search)
+        if search.blank?
+            Stock.all
+        else
+            where('name LIKE ?', "%#{search}%")
+        end
+    end
 end
