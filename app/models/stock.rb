6b59d34626e47c6b40d468151875b9656757949a
Kunal <kdshah6593@gmail.com>
2021-02-01 17:34:09 -0500

    cleaned up view page with stock methods

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -11,4 +11,33 @@ class Stock < ApplicationRecord
             where('name LIKE ?', "%#{search}%")
         end
     end
+
+    # This API limits requests to 5 per minute and 500 per day; need premium membership to accomodate more requests
+    def stock_info
+        response = HTTParty.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{self.symbol}&apikey=#{ENV["STOCK_API_KEY"]}")
+    end
+
+    def stock_price
+        stock_info["Global Quote"]["05. price"]
+    end
+
+    def stock_high
+        stock_info["Global Quote"]["03. high"]
+    end
+
+    def stock_low
+        stock_info["Global Quote"]["04. low"]
+    end
+
+    def stock_open
+        stock_info["Global Quote"]["02. open"]
+    end
+
+    def stock_close
+        stock_info["Global Quote"]["08. previous close"]
+    end
+
+    def stock_percent_change
+        stock_info["Global Quote"]["10. change percent"]
+    end
 end
