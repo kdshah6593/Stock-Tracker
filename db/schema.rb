a0c0cc07b5d2e81da48f80c762c005b2d716529f
Kunal <kdshah6593@gmail.com>
2021-02-02 14:34:29 -0500

    uid and provider column for omniauth use

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_31_195808) do
+ActiveRecord::Schema.define(version: 2021_02_02_193233) do
 
   create_table "portfolios", force: :cascade do |t|
     t.string "title"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 2021_01_31_195808) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
+    t.string "provider"
   end
 
   create_table "watchlists", force: :cascade do |t|
