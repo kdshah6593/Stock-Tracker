73ee1b63d46a1c55b425522722469d536e543152
Kunal <kdshah6593@gmail.com>
2021-02-04 21:53:19 -0500

    add custom validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,22 @@
 class User < ApplicationRecord
+    include ActiveModel::Validations
     has_secure_password
+
     has_many :watchlists, dependent: :destroy
     has_many :portfolios, dependent: :destroy
     has_many :stock_purchases, through: :portfolios
     has_many :stocks, through: :stock_purchases
+
     validates :username, presence: true
     validates :username, uniqueness: true, on: :create
+    validates_with usernameValidator
     validates :password, length: { in: 6..20 }
+    validates_with passwordValidator
     validates :first_name, presence: true, on: :create
     validates :last_name, presence: true, on: :create
     validates :email, presence: true, on: :create
     validates :email, uniqueness: true, on: :create
+    validates_with emailValidator
 
     def portfolio_count
         self.portfolios.size
