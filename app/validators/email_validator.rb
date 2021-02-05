e2d595b0667dd17f2b3706e2ea5ad6052adf32a0
Kunal <kdshah6593@gmail.com>
2021-02-04 21:51:01 -0500

    email validator

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,5 +1,7 @@
-!params[:user][:email].match(/\A[^@\s]+@[^@\s]+\z/)
-
 class emailValidator < ActiveModel::Validator
-    
+    def validate(record)
+        unless record.email.match(/\A[^@\s]+@[^@\s]+\z/)
+            record.errors.add :email, "Please give a valid email address!"
+        end
+    end
 end
